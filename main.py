c41bf1e7b0ac48c44fa799718d8fb3094b120f7a
whaytogo <whaytogo@gmail.com>
2024-06-17 19:38:35 +0400

    Final version before submit

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 
 def main():
-    # print_book("books/frankenstein.txt")   
+    # print the report header
     print("---Begin report of books/frankenstein.txt ---")
 
-    
+    # print word count in book
     book_string = stringify_book("books/frankenstein.txt")
     words_count = (word_count(book_string))
-    print(f"{words_count} words found in the document")
+    print(f"{words_count} words found in the document\n")
 
+    # count letters and print letter count 
     letter_count = (letters_count(book_string))
     for item in letter_count:
-        print(f"The {item} character was found {letter_count[item]} times")
+        if item.isalpha() and item != ' ':
+            print(f"The '{item}' character was found {letter_count[item]} times")
+
+    # print the report footer
+    print("---End of report---")
 
 def print_book(book_url):
     
@@ -27,10 +32,28 @@ def word_count(string):
 def letters_count(string):
     string = string.lower()
     letter_count = {}
+    # fill the dictionary with letter counts
     for char in string:
         letter_count[char] = letter_count.get(char, 0) + 1
 
-    return letter_count
+    # make the dict an array of dicts so it's sortable by value
+    letter_list = []
+    for letter in letter_count:
+        letter_list.append({"letter": letter, "count": letter_count[letter]})
+
+    letter_list.sort(reverse=True, key=sort_dict)
+    final_dict = {}
+    for dict in letter_list:
+        final_dict.update({dict["letter"]: dict["count"]})
+
+    return final_dict
+
+
+def sort_dict(dict):
+    return dict["count"]
+
+
+
 
 
 main()
